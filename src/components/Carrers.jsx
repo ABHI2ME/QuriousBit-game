322f7db10678b581af75b530adf7806f739962f2
ABHI2ME <abhignoneclsingh@gmail.com>
2024-09-16 01:46:15 +0530

    corrected press realease  component

---
+++ src/components/Carrers.jsx
@@ -0,0 +1,16 @@
+import React from 'react' ;
+import styles from "./carrers.module.css" ;
+
+const Carrers = () => {
+  return (
+   <>
+       <div className={styles.carrers}>
+            <div className={styles.heading}>
+                
+            </div>
+       </div>
+   </>
+  )
+}
+
+export default Carrers
--- src/components/PressRelease.jsx
+++ src/components/PressRelease.jsx
@@ -24,18 +24,18 @@ const PressRelease = () => {
                              </div>
                              <div className={styles.founderNews1}>
                                  <div className={styles.news1Text}>
-                                 QuriousBit bags $2 million funding from Lumikai to develop casual mobile Games
+                                 QuriousBit to utilise $2 million seed fundraise for hiring and developing tech infrastructure
                                  </div>
                                  <div className={styles.news1Link}>
-                                    <a href='https://inc42.com/buzz/quriousbit-bags-funding-from-lumikai-to-develop-casual-mobile-games/'>Featured in INC42</a>
+                                    <a href='https://www.cnbctv18.com/startup/startup-fundraise-quriousbit-gaming-vahdam-india-tea-17898191.htm/'>Featured in CNBC</a>
                                  </div>
                              </div>
                              <div className={styles.founderNews1}>
                                  <div className={styles.news1Text}>
-                                 QuriousBit bags $2 million funding from Lumikai to develop casual mobile Games
+                                 Casual gaming studio QuriousBit bags $2 million funding from Lumikai, General Catalyst
                                  </div>
                                  <div className={styles.news1Link}>
-                                    <a href='https://inc42.com/buzz/quriousbit-bags-funding-from-lumikai-to-develop-casual-mobile-games/'>Featured in INC42</a>
+                                    <a href='https://www.moneycontrol.com/news/business/startup/casual-gaming-studio-quriousbit-bags-2-million-funding-from-lumikai-general-catalyst-11437971.html/'>Featured in Moneycontrol</a>
                                  </div>
                              </div>
                              
